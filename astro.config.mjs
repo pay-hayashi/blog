7390df942e56cc3e49df309eec0ec4b6a9e0b36a
pay-hayashi <junlaurantlaurant@gmail.com>
2023-12-29 19:27:02 +0900

    fix: base url を設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://pay-hayashi.github.io',
+  base: '/blog',
+  integrations: [tailwind()],
 });
